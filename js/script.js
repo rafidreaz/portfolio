4047df42e9f0218e20e7a44f509a9dfd752469ee
Md Rafid Reaz <rafid.reaz@nrbbankbd.com>
2025-01-05 12:42:03 +0600

    Added profile picture with tilt animation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // Initialize AOS animations
 AOS.init();
+
+VanillaTilt.init(document.querySelectorAll('[data-tilt]'), {
+    max: 15,           // Tilt angle limit
+    speed: 800,        // Smooth transition speed
+    glare: true,       // Enable glare
+    'max-glare': 0.3,  // Set maximum glare intensity
+    scale: 1.1         // Slight image scale on hover for added smoothness
+  });
