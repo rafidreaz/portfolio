3980dc637be25b9873fc96d985966adb3b2dca02
Md Rafid Reaz <rafid.reaz@nrbbankbd.com>
2025-01-05 18:36:10 +0600

    a

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 // Initialize AOS animations
-AOS.init();
-
-VanillaTilt.init(document.querySelectorAll('[data-tilt]'), {
-    max: 15,           // Tilt angle limit
-    speed: 800,        // Smooth transition speed
-    glare: true,       // Enable glare
-    'max-glare': 0.3,  // Set maximum glare intensity
-    scale: 1.1         // Slight image scale on hover for added smoothness
-  });
-
- 
+AOS.init(); 
 
   document.addEventListener('DOMContentLoaded', () => {
     const profilePic = document.getElementById('profile-pic');
