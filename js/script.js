2c6d99a8e660c13649841cddbce7036923b2d6f2
Md Rafid Reaz <rafid.reaz@nrbbankbd.com>
2025-01-06 11:34:45 +0600

    alignment fix

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // Initialize AOS animations
 AOS.init(); 
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const profilePic = document.getElementById('profile-pic');
-    const socialLinks = document.getElementById('social-links');
-  
-    profilePic.addEventListener('click', () => {
-      socialLinks.classList.toggle('active');
-    });
-  
-    document.addEventListener('click', (event) => {
-      if (!profilePic.contains(event.target) && !socialLinks.contains(event.target)) {
-        socialLinks.classList.remove('active');
-      }
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const profilePic = document.getElementById('profile-pic');
+  const socialLinks = document.getElementById('social-links');
+
+  profilePic.addEventListener('click', () => {
+    socialLinks.classList.toggle('active');
   });
+
+  document.addEventListener('click', (event) => {
+    if (!profilePic.contains(event.target) && !socialLinks.contains(event.target)) {
+      socialLinks.classList.remove('active');
+    }
+  });
+});
   
